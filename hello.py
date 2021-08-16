9e3dae37bc9273c9d1bb3360e08fdeadee909b95
ebitezion <ebitezion@gmail.com>
2021-08-16 20:02:14 +0100

    stdout with print func

---
+++ hello.py
@@ -0,0 +1,11 @@
+''' Our formal induction to python'''
+#this line is a simple comment
+print("Hello universe") 
+print("---------------------------")
+#variables and formating
+name="Zion"
+print(f"this gets interesing fast, i am {name}")
+print("------------------------")
+#another type of print method using special chars
+print("Meow",end="\t")
+print("meow",end="\n")
